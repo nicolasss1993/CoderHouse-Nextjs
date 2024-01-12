3c009bb6b6a0df5c83c34c41624d1ee76c2251b0
Nicolas <niqo5220@gmail.com>
2024-01-12 08:57:47 -0300

    upload

--- components/products/ProductDetail.jsx
+++ components/products/ProductDetail.jsx
@@ -1,38 +1,55 @@
-import Image from "next/image";
+"use client"
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
 import AmountSelect from '@/components/products/AmountSelect';
-import { storage, db } from "@/utils/firebase";
+import { storage, db } from '@/utils/firebase';
 import { ref, getDownloadURL } from 'firebase/storage';
-import { doc, updateDoc } from "firebase/firestore"
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 
 export const AddProductToCar = async (uid, cart) => {
     const docRef = doc(db, "cart", uid);
     return updateDoc(docRef, { cart: cart });
 };
 
-const ProductDetail = async ({ slug }) => {
-    const url = `${process.env.VERCEL_URL}/api/productos/detail/${slug}`;
-    const item = await fetch(url, {
-        cache: 'no-store'
-    }).then(res => res.json())
+const ProductDetail = ({ slug }) => {
+    const [item, setItem] = useState(null);
 
-    const imageUrl = await getDownloadURL(ref(storage, `products/${slug}.webp`));
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const url = `${process.env.VERCEL_URL}/api/productos/detail/${slug}`;
+                const response = await fetch(url, { cache: 'no-store' });
+                const itemData = await response.json();
+
+                const imageUrl = await getDownloadURL(ref(storage, `products/${slug}.webp`));
+
+                setItem({
+                    ...itemData,
+                    imageUrl,
+                });
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setItem(null);
+            }
+        };
+
+        fetchData();
+    }, [slug]);
+
+    if (!item) {
+        return null;
+    }
 
     return (
         <div className="max-w-4xl m-auto">
             <section className="flex gap-6">
                 <div className="relative basis-1/2">
-                    <Image
-                        src={imageUrl}
-                        alt={item.title}
-                        width={860}
-                        height={860}
-                    />
+                    <Image src={item.imageUrl} alt={item.title} width={860} height={860} />
                 </div>
                 <div className="basis-1/2">
                     <h2 className="text-2xl font-semibold border-b border-gray-200 pb-4 mb-4">{item.title}</h2>
                     <p className="text-4xl">$ {item.price}</p>
-
-                    <AmountSelect item={item} />
+                    <AmountSelect item={item} addToCart={() => addToCart('usuario_id', 'nuevo_carrito')} />
                 </div>
             </section>
             <section className="mt-12">
@@ -40,7 +57,7 @@ const ProductDetail = async ({ slug }) => {
                 <p className="text-gray-600">{item.description}</p>
             </section>
         </div>
-    )
-}
+    );
+};
 
-export default ProductDetail
+export default ProductDetail;
