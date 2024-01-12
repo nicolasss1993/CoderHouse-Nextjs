b00fe90de0ecc83b5ff07685795dad3b8ed60105
Nicolas <niqo5220@gmail.com>
2024-01-12 10:34:40 -0300

    upload

--- components/products/ProductDetail.jsx
+++ components/products/ProductDetail.jsx
@@ -1,4 +1,3 @@
-//import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import AmountSelect from '@/components/products/AmountSelect';
 import { storage } from '@/utils/firebase';
@@ -6,39 +5,8 @@ import { ref, getDownloadURL } from 'firebase/storage';
 import { getDetail } from '@/utils/constants';
 
 const ProductDetail = async ({ slug }) => {
-    //const [item, setItem] = useState(null);
     console.log("ProductDetail ", slug)
-    const item = getDetail(slug);
-    /*useEffect(() => {
-        const fetchData = async () => {
-            try {
-                console.log("Entre al Effect")
-                const url = `${process.env.VERCEL_URL}/api/productos/detail/${slug}`;
-                const response = await fetch(url, { cache: 'no-store' });
-                console.log("Response del fetch ", response);
-                const itemData = await response.json();
-                console.log("ItemData ", itemData);
-
-                const imageUrl = await getDownloadURL(ref(storage, `products/${slug}.webp`));
-
-                setItem({
-                    ...itemData,
-                    imageUrl,
-                });
-                console.log('Item Ultimo ', item);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-                setItem(null);
-            }
-        };
-
-        fetchData();
-    }, [slug, item]);*/
-
-    /*if (!item) {
-        console.log("sss");
-        return null;
-    }*/
+    const item = await getDetail(slug);
     console.log(item)
     return (
         <div className="max-w-4xl m-auto">
@@ -46,11 +14,11 @@ const ProductDetail = async ({ slug }) => {
                 <div className="relative basis-1/2">
                     <Image src={item.imageUrl} alt={item.title} width={860} height={860} />
                 </div>
-                <div className="basis-1/2">
+                {/*<div className="basis-1/2">
                     <h2 className="text-2xl font-semibold border-b border-gray-200 pb-4 mb-4">{item.title}</h2>
                     <p className="text-4xl">$ {item.price}</p>
                     <AmountSelect item={item} />
-                </div>
+                </div>*/}
             </section>
             <section className="mt-12">
                 <h3 className="text-xl font-semibold border-b border-gray-200 pb-4 my-4">Descripcion</h3>
